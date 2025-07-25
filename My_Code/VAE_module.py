093bd03bbb58bc91fb057ae1a93323f51a3390bb
Tanveer <1905025@ugrad.cse.buet.ac.bd>
2025-07-25 20:18:47 +0600

    vae added

--- My_Code/VAE_module.py
+++ My_Code/VAE_module.py
@@ -2,24 +2,25 @@ import tensorflow as tf
 from tensorflow.keras import layers, Model
 
 class VAE(Model):
-    def __init__(self, input_dim, latent_dim=16, **kwargs):
-        super(VAE, self).__init__(**kwargs)
+    def __init__(self, input_dim, latent_dim=32):
+        super(VAE, self).__init__()
         self.latent_dim = latent_dim
+        
+        # Encoder network
         self.encoder = tf.keras.Sequential([
-            layers.Input(shape=(input_dim,)),
+            layers.InputLayer(input_shape=(input_dim,)),
             layers.Dense(64, activation='relu'),
             layers.Dense(32, activation='relu'),
-            layers.Dense(latent_dim + latent_dim)  # mean and log_var
+            layers.Dense(latent_dim * 2),  # outputs mean and log variance
         ])
+
+        # Decoder network
         self.decoder = tf.keras.Sequential([
-            layers.Input(shape=(latent_dim,)),
+            layers.InputLayer(input_shape=(latent_dim,)),
             layers.Dense(32, activation='relu'),
             layers.Dense(64, activation='relu'),
-            layers.Dense(input_dim, activation='sigmoid')
+            layers.Dense(input_dim, activation='sigmoid'),  # sigmoid for binary data
         ])
-        self.total_loss_tracker = tf.keras.metrics.Mean(name="loss")
-        self.reconstruction_loss_tracker = tf.keras.metrics.Mean(name="recon_loss")
-        self.kl_loss_tracker = tf.keras.metrics.Mean(name="kl_loss")
 
     def encode(self, x):
         z = self.encoder(x)
@@ -28,7 +29,7 @@ class VAE(Model):
 
     def reparameterize(self, z_mean, z_log_var):
         eps = tf.random.normal(shape=tf.shape(z_mean))
-        return eps * tf.exp(0.5 * z_log_var) + z_mean
+        return eps * tf.exp(z_log_var * 0.5) + z_mean
 
     def decode(self, z):
         return self.decoder(z)
@@ -36,58 +37,52 @@ class VAE(Model):
     def call(self, x):
         z_mean, z_log_var = self.encode(x)
         z = self.reparameterize(z_mean, z_log_var)
-        return self.decode(z)
+        x_recon = self.decode(z)
+        return x_recon
 
-    def train_step(self, data):
-        x = data[0] if isinstance(data, tuple) else data
-        x = tf.cast(x, tf.float32)
-
-        with tf.GradientTape() as tape:
-            z_mean, z_log_var = self.encode(x)
-            z = self.reparameterize(z_mean, z_log_var)
-            x_recon = self.decode(z)
-
-            recon_loss = tf.reduce_mean(tf.keras.losses.mse(x, x_recon))
-            kl_loss = -0.5 * tf.reduce_mean(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
-            total_loss = recon_loss + kl_loss
-
-        grads = tape.gradient(total_loss, self.trainable_weights)
-        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
-
-        self.total_loss_tracker.update_state(total_loss)
-        self.reconstruction_loss_tracker.update_state(recon_loss)
-        self.kl_loss_tracker.update_state(kl_loss)
-
-        return {
-            "loss": self.total_loss_tracker.result(),
-            "recon_loss": self.reconstruction_loss_tracker.result(),
-            "kl_loss": self.kl_loss_tracker.result()
-        }
-
-    def test_step(self, data):
-        x = data[0] if isinstance(data, tuple) else data
-        x = tf.cast(x, tf.float32)
-
+    def compute_loss(self, x):
         z_mean, z_log_var = self.encode(x)
         z = self.reparameterize(z_mean, z_log_var)
         x_recon = self.decode(z)
 
-        recon_loss = tf.reduce_mean(tf.keras.losses.mse(x, x_recon))
-        kl_loss = -0.5 * tf.reduce_mean(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
-        total_loss = recon_loss + kl_loss
+        # Binary crossentropy reconstruction loss summed over features
+        recon_loss = tf.reduce_mean(
+            tf.reduce_sum(
+                tf.keras.losses.binary_crossentropy(x, x_recon), axis=1
+            )
+        )
 
-        # Update metrics to track validation losses
-        self.total_loss_tracker.update_state(total_loss)
-        self.reconstruction_loss_tracker.update_state(recon_loss)
-        self.kl_loss_tracker.update_state(kl_loss)
+        # KL divergence
+        kl_loss = -0.5 * tf.reduce_mean(
+            tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=1)
+        )
+
+        total_loss = recon_loss + kl_loss
+        return total_loss, recon_loss, kl_loss
+
+    def train_step(self, data):
+        x = tf.cast(data, tf.float32)
+        with tf.GradientTape() as tape:
+            total_loss, recon_loss, kl_loss = self.compute_loss(x)
+        grads = tape.gradient(total_loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+
+        self.compiled_metrics.update_state(x, self.call(x))
 
         return {
-            "loss": self.total_loss_tracker.result(),
-            "recon_loss": self.reconstruction_loss_tracker.result(),
-            "kl_loss": self.kl_loss_tracker.result()
+            "loss": total_loss,
+            "recon_loss": recon_loss,
+            "kl_loss": kl_loss,
         }
 
+    def test_step(self, data):
+        x = tf.cast(data, tf.float32)
+        total_loss, recon_loss, kl_loss = self.compute_loss(x)
 
-    @property
-    def metrics(self):
-        return [self.total_loss_tracker, self.reconstruction_loss_tracker, self.kl_loss_tracker]
+        self.compiled_metrics.update_state(x, self.call(x))
+
+        return {
+            "loss": total_loss,
+            "recon_loss": recon_loss,
+            "kl_loss": kl_loss,
+        }
