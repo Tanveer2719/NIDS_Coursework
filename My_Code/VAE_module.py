64e873da2d72e0d411fc14cf39ff7c1e34f7b47e
Tanveer <1905025@ugrad.cse.buet.ac.bd>
2025-07-25 19:49:10 +0600

    vae added

--- My_Code/VAE_module.py
+++ My_Code/VAE_module.py
@@ -1,34 +1,24 @@
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras.models import Model
-from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers, Model
 
-
-class VAE(tf.keras.Model):
+class VAE(Model):
     def __init__(self, input_dim, latent_dim=16, **kwargs):
         super(VAE, self).__init__(**kwargs)
         self.latent_dim = latent_dim
-
-        # Encoder
         self.encoder = tf.keras.Sequential([
             layers.Input(shape=(input_dim,)),
             layers.Dense(64, activation='relu'),
             layers.Dense(32, activation='relu'),
-            layers.Dense(latent_dim * 2)  # mean and log var
+            layers.Dense(latent_dim + latent_dim)  # mean and log_var
         ])
-
-        # Decoder
         self.decoder = tf.keras.Sequential([
             layers.Input(shape=(latent_dim,)),
             layers.Dense(32, activation='relu'),
             layers.Dense(64, activation='relu'),
             layers.Dense(input_dim, activation='sigmoid')
         ])
-
-        # Track losses for logging
-        self.total_loss_tracker = tf.keras.metrics.Mean(name="total_loss")
-        self.recon_loss_tracker = tf.keras.metrics.Mean(name="recon_loss")
+        self.total_loss_tracker = tf.keras.metrics.Mean(name="loss")
+        self.reconstruction_loss_tracker = tf.keras.metrics.Mean(name="recon_loss")
         self.kl_loss_tracker = tf.keras.metrics.Mean(name="kl_loss")
 
     def encode(self, x):
@@ -38,18 +28,20 @@ class VAE(tf.keras.Model):
 
     def reparameterize(self, z_mean, z_log_var):
         eps = tf.random.normal(shape=tf.shape(z_mean))
-        return eps * tf.exp(z_log_var * 0.5) + z_mean
+        return eps * tf.exp(0.5 * z_log_var) + z_mean
 
     def decode(self, z):
         return self.decoder(z)
 
-    def call(self, inputs):
-        z_mean, z_log_var = self.encode(inputs)
+    def call(self, x):
+        z_mean, z_log_var = self.encode(x)
         z = self.reparameterize(z_mean, z_log_var)
         return self.decode(z)
 
     def train_step(self, data):
-        x = tf.cast(data, tf.float32)
+        x = data[0] if isinstance(data, tuple) else data
+        x = tf.cast(x, tf.float32)
+
         with tf.GradientTape() as tape:
             z_mean, z_log_var = self.encode(x)
             z = self.reparameterize(z_mean, z_log_var)
@@ -59,21 +51,23 @@ class VAE(tf.keras.Model):
             kl_loss = -0.5 * tf.reduce_mean(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
             total_loss = recon_loss + kl_loss
 
-        grads = tape.gradient(total_loss, self.trainable_variables)
-        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
 
         self.total_loss_tracker.update_state(total_loss)
-        self.recon_loss_tracker.update_state(recon_loss)
+        self.reconstruction_loss_tracker.update_state(recon_loss)
         self.kl_loss_tracker.update_state(kl_loss)
 
         return {
             "loss": self.total_loss_tracker.result(),
-            "recon_loss": self.recon_loss_tracker.result(),
-            "kl_loss": self.kl_loss_tracker.result(),
+            "recon_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result()
         }
 
     def test_step(self, data):
-        x = tf.cast(data, tf.float32)
+        x = data[0] if isinstance(data, tuple) else data
+        x = tf.cast(x, tf.float32)
+
         z_mean, z_log_var = self.encode(x)
         z = self.reparameterize(z_mean, z_log_var)
         x_recon = self.decode(z)
@@ -85,5 +79,9 @@ class VAE(tf.keras.Model):
         return {
             "loss": total_loss,
             "recon_loss": recon_loss,
-            "kl_loss": kl_loss,
+            "kl_loss": kl_loss
         }
+
+    @property
+    def metrics(self):
+        return [self.total_loss_tracker, self.reconstruction_loss_tracker, self.kl_loss_tracker]
